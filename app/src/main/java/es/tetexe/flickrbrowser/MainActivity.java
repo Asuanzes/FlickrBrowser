8ac39180dbd433c850a5ec9f5d7432781b6376f0
Shochlves <coco1970@gmail.com>
2015-08-21 21:28:53 +0200

    Deshecho cambio var

--- app/src/main/java/es/tetexe/flickrbrowser/MainActivity.java
+++ app/src/main/java/es/tetexe/flickrbrowser/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    String mNombre;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
