3827ef2360c18d054835bb696abdc725df157b32
Shochlves <coco1970@gmail.com>
2015-08-21 21:18:02 +0200

    GetRawData.class

--- app/src/main/java/es/tetexe/flickrbrowser/GetRawData.java
+++ app/src/main/java/es/tetexe/flickrbrowser/GetRawData.java
@@ -1,5 +1,16 @@
 package es.tetexe.flickrbrowser;
 
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by Alejandro on 21/08/2015.
  */
@@ -22,5 +33,100 @@ public class GetRawData {
 
     public GetRawData(String mRawUrl) {
         this.mRawUrl = mRawUrl;
+        this.mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+
+    public void reset() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        this.mRawUrl = null;
+        this.mData = null;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+
+    public DownloadStatus getmDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+
+    public void execute(){
+
+        this.mDownloadStatus = DownloadStatus.PROCESSING;
+        DownloadRawData downloadRawData = new DownloadRawData();
+        downloadRawData.execute(mRawUrl);
+
+    }
+
+
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+
+        protected void onPostExecute(String webData) {
+
+            mData = webData;
+            Log.v(LOG_TAG, "Data returnned: " + mData);
+
+            if (mData == null){
+                if (mRawUrl == null){
+                    mDownloadStatus = DownloadStatus.NOT_INITIALISED;
+                }   else {
+                    mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+                }
+            } else{
+                //succes
+                mDownloadStatus = DownloadStatus.OK;
+            }
+
+
+        }
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            if (params == null)
+                return null;
+
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    return null;
+                }
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                return buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally {
+
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+
+                }
+            }
+
+        }
+
     }
 }
--- app/src/main/java/es/tetexe/flickrbrowser/MainActivity.java
+++ app/src/main/java/es/tetexe/flickrbrowser/MainActivity.java
@@ -1,7 +1,7 @@
 package es.tetexe.flickrbrowser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GetRawData theData =
+                new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
+        theData.execute();
+
     }
 
 
